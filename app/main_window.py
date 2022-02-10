b98c883121c22ff13c73719714fe9a8d592df70b
dvraklanov <raklanov.danil.v@gmail.com>
2022-02-10 23:26:35 +0300

    Отображение в таблице данных из базы

--- app/main_window.py
+++ app/main_window.py
@@ -1,7 +1,10 @@
-from PySide6.QtWidgets import QMainWindow
+import logging
+
+from PySide6.QtWidgets import QMainWindow, QTableWidgetItem
 
 from .ui.ui_main_window import Ui_MainWindow
-
+from .vehicles import Vehicles
+from .database import Database
 
 # главное окно приложения расширяет класс QWidget
 class MainWindow(QMainWindow):
@@ -11,4 +14,20 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.db = Database("app/data/test_db")
+        self.vehicle_data = Vehicles(self.db)
+        self.set_veh_table()
+
+    def set_veh_table(self):
+
+        all_veh = self.vehicle_data.get_all_veh()
+        logging.info(all_veh)
+
+        for i, vehicle in enumerate(all_veh):
+
+            self.ui.veh_table.insertRow(i)
+            self.ui.veh_table.setItem(i, 0, QTableWidgetItem(str(vehicle[1])))
+            self.ui.veh_table.setItem(i, 1, QTableWidgetItem(str(self.vehicle_data.status_list[vehicle[2]])))
+
+
 
