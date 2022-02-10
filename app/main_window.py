c6b46e3700c15278b42a86dc6e4cee7f4581c445
dvraklanov <raklanov.danil.v@gmail.com>
2022-02-10 23:34:25 +0300

    Микро изменения, пошел спать

--- app/main_window.py
+++ app/main_window.py
@@ -6,6 +6,7 @@ from .ui.ui_main_window import Ui_MainWindow
 from .vehicles import Vehicles
 from .database import Database
 
+
 # главное окно приложения расширяет класс QWidget
 class MainWindow(QMainWindow):
 
@@ -14,20 +15,19 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        self.ui.veh_table.setColumnWidth(0, 450)
+        self.ui.veh_table.setColumnWidth(1, 50)
+
         self.db = Database("app/data/test_db")
         self.vehicle_data = Vehicles(self.db)
         self.set_veh_table()
 
-    def set_veh_table(self):
-
-        all_veh = self.vehicle_data.get_all_veh()
+    def set_veh_table(self, filter={}):
+        all_veh = self.vehicle_data.get_all_veh(filter=filter)
         logging.info(all_veh)
 
         for i, vehicle in enumerate(all_veh):
-
             self.ui.veh_table.insertRow(i)
             self.ui.veh_table.setItem(i, 0, QTableWidgetItem(str(vehicle[1])))
             self.ui.veh_table.setItem(i, 1, QTableWidgetItem(str(self.vehicle_data.status_list[vehicle[2]])))
-
-
-
